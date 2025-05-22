46effea3a589ca1267e12f8865600faa76bbf78c
arishwarang@gmail.com <arishwarang@gmail.com>
2025-05-22 12:01:33 +0000

    latest

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch('/hello')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('message').textContent = data.message;
+    })
+    .catch(error => {
+        console.error('Error fetching message:', error);
+    });
